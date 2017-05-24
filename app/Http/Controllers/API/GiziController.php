69fe9f7de229694509e394175c9f2fd1d24b8bc1
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-25 02:47:52 +0700

    Add Gizi API
    - Get All
    - Get One

---
+++ app/Http/Controllers/API/GiziController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use DB;
+
+use App\Gizi;
+use App\Ingredient;
+
+class GiziController extends Controller
+{
+  public function getAll() {
+    $responses = Gizi::with('ingredient')->get();
+    return response()->json($responses);
+  }
+
+  public function getOne($id) {
+    $responses = Gizi::find($id);
+    return response()->json($responses);
+  }
+}
